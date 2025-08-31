431a074d3159840d8f43038ed2d6a1e00542d40c
SHEFFmachine <sheff@sheffmachine.com>
2025-08-31 12:06:08 -0500

    KSB-1 Add basic into

---
+++ src/main/kotlin/com/sheffmachine/kotlinbootproject/KotlinBootProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.sheffmachine.kotlinbootproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBootProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinBootProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/sheffmachine/kotlinbootproject/KotlinBootProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sheffmachine.kotlinbootproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBootProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
